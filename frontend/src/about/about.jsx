a79b771f707018a747cd12771fb0bad154c56cb6
rafadevx <rafa.comp@gmail.com>
2018-07-17 14:35:32 -0300

    finalizado reac

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Tarefas' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor..</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor...</p>
+    </div>
+)
